a4e297444972267d86c9147b94e63bdfc2db588c
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-03-27 23:59:31 +0500

    'Superkeyword'

--- src/NewUser.java
+++ src/NewUser.java
@@ -1,22 +1,38 @@
 
 public abstract class NewUser {
 
-	public String firstName;
-	public String lastName;
+//	public String firstName;
+//	public String lastName;
 	public boolean verified = false;
 	
-	//Creating aMethod in This class and then overriding it in Derived class (Virtual Java)
+	//Creating a Method in this class and then overriding it in Derived class (Virtual Java)
 	//Note: Static and private  methods are not virtual
 	
 //	public void sayHello() {
 //		System.out.println("Hi! I'm a user. My name is " + firstName +" " + lastName);
 //	}
 	
-	//abstract classes - this prevent us to instantiate the class (the only way to get vlue from abstract classes is to create extended classes )
+	//abstract classes - this prevent us to instantiate the class (the only way to get value from abstract classes is to create extended classes )
 	// public abstract class NewUser{} - syntax
 	
 	//Abstract method - it does not allow to pass body in the method 
 	// public abstract void sayHello();	-- but we can pass different bodies of this class to its extended classes
 	
-	public abstract void sayHello();
+//	public abstract void sayHello();
+	
+	//Invoke class with super keyword
+	public void sayHello() {
+//		System.out.println("User Version");
+		System.out.println(firstName+ " " + lastName);
+	}
+	
+	//read only fields assigned with constructors
+	private String firstName;
+	private String lastName;
+	
+	public NewUser(String fn, String ln) {
+		firstName = fn;
+		lastName = ln;
+	}
+	
 }
---
+++ src/Superkeyword.java
@@ -0,0 +1,11 @@
+
+public class Superkeyword {
+
+	public static void main(String[] args) {
+		
+		//Invoke a parent class with super keyword - if we need a logic of parent class in extended one so we use super keyword in child class
+		Students s = new Students("Caleb","Curry");
+		s.sayHello();
+	}
+
+}
